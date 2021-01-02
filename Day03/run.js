82befccff9d8dc84816ca5f2d3d0380becfed6e4
mikeday37 <mikeday370146@gmail.com>
2021-01-01 23:07:28 -0500

    more cleanup

--- Day03/run.js
+++ Day03/run.js
@@ -21,7 +21,7 @@ let examples = [{
     },{
         rawInput: 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7',
         answer: 135
-    },{
+    },{ // official examples above this line, manual below -- simple but turned out very helpful
         rawInput: 'R1,U2,R2\nU1,R2,U2',
         answer: 2
     },{
